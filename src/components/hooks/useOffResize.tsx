532915d2b96499346e611168c494bde0edd75edc
shdomi8599 <shdomi@naver.com>
2023-06-14 02:26:55 +0900

    feat : useOffResize hook

---
+++ src/components/hooks/useOffResize.tsx
@@ -0,0 +1,36 @@
+import { Dispatch, SetStateAction, useEffect } from "react";
+
+export const useOffResize = (
+  size: number,
+  set: "up" | "down",
+  setEvent: Dispatch<SetStateAction<boolean>>
+) => {
+  const upOffMenu = () => {
+    if (window.innerWidth > size) {
+      setEvent(false);
+    }
+  };
+
+  const downOffMenu = () => {
+    if (window.innerWidth < size) {
+      setEvent(false);
+    }
+  };
+
+  useEffect(() => {
+    if (set === "up") {
+      window.addEventListener("resize", upOffMenu);
+    }
+    if (set === "down") {
+      window.addEventListener("resize", downOffMenu);
+    }
+    return () => {
+      if (set === "up") {
+        window.removeEventListener("resize", upOffMenu);
+      }
+      if (set === "down") {
+        window.removeEventListener("resize", downOffMenu);
+      }
+    };
+  }, []);
+};
