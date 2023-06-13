ef1720e60662966b2e596669fe6004c88019a668
shdomi8599 <shdomi@naver.com>
2023-06-13 20:50:58 +0900

    feat : routes 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import "./App.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import HomePage from "./components/pages/HomePage";
+import QuizPage from "./components/pages/QuizPage";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+
 function App() {
-  return <div>ss</div>;
+  //라우트에 들어갈 데이터들
+  const routes = [
+    { path: "/", element: <HomePage /> },
+    { path: "/quiz/:id", element: <QuizPage /> },
+  ];
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        {routes.map((el) => (
+          <Route key={el.path} path={el.path} element={el.element} />
+        ))}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
