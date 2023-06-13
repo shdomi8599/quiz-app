3a4b6edda34ce04c3f5515989e7bcadbd47a6de2
shdomi8599 <shdomi@naver.com>
2023-06-14 02:28:15 +0900

    feat : 스타일 컴포넌트 Theme

---
+++ src/styels/theme.jsx
@@ -0,0 +1,24 @@
+const breakpoints = {
+  mobile: "576px",
+  tablet: "768px",
+  desktop: "1200px",
+};
+
+const mediaQueries = {
+  mobile: `(max-width: ${breakpoints.mobile})`,
+  tablet: `(max-width: ${breakpoints.tablet})`,
+  desktop: `(max-width: ${breakpoints.desktop})`,
+};
+
+const colors = {
+  main: "#001529",
+  mainHover: "#1677ff",
+  sub: "#72e9df",
+};
+
+const theme = {
+  mediaQueries,
+  colors,
+};
+
+export default theme;
