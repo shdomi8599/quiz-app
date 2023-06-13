72decfe78a83775c4539858a40634f7ffc74e3c3
shdomi8599 <shdomi@naver.com>
2023-06-14 06:25:44 +0900

    refactor : constants 데이터 보완

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,4 +1,5 @@
 import HomePage from "../components/pages/HomePage";
+import QuizPage from "../components/pages/QuizPage";
 import RankingPage from "../components/pages/RankingPage";
 import StatisticsPage from "../components/pages/StatisticsPage";
 import MistakesPage from "../components/pages/MistakesPage";
@@ -6,6 +7,7 @@ import RetryPage from "../components/pages/RetryPage";
 
 export const ROUTES = [
   { path: "/", element: <HomePage /> },
+  { path: "/quiz/:id", element: <QuizPage /> },
   { path: "/ranking", element: <RankingPage /> },
   { path: "/statistics", element: <StatisticsPage /> },
   { path: "/mistakes", element: <MistakesPage /> },
@@ -13,6 +15,7 @@ export const ROUTES = [
 ];
 
 export const NAV_ITEMS: { [key: string]: string } = {
+  홈: "/",
   랭킹: "/ranking",
   통계: "/statistics",
   오답노트: "/mistakes",
