e597b5c2464be32593273abbd011e85344f2867b
shdomi8599 <shdomi@naver.com>
2023-06-14 12:09:50 +0900

    feat : quizDatasState, wrongAnswerQuestionsState, wrongAnswerQuestionsCountState

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,4 +1,6 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+
+import { QuizData } from "../types";
 
 export const userIdState = atom({
   key: "userIdState",
@@ -7,5 +9,23 @@ export const userIdState = atom({
 
 export const currentNavItemState = atom({
   key: "currentNavItemState",
-  default: "",
+  default: "홈",
+});
+
+export const quizDatasState = atom<QuizData[]>({
+  key: "quizDatasState",
+  default: [],
+});
+
+export const wrongAnswerQuestionsState = atom<QuizData[]>({
+  key: "wrongAnswerQuestionsState",
+  default: [],
+});
+
+export const wrongAnswerQuestionsCountState = selector({
+  key: "wrongAnswerQuestionsCountState",
+  get: ({ get }) => {
+    const wrongAnswerQuestions = get(wrongAnswerQuestionsState);
+    return wrongAnswerQuestions.length;
+  },
 });
