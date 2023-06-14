e45d1be6858dbee8dafa01102b133247ef9ec8fb
shdomi8599 <shdomi@naver.com>
2023-06-14 20:17:37 +0900

    refactor : 결과표 데이터 quiz페이지 관리로 인해 로직 변경

--- src/components/pages/ResultPage.tsx
+++ src/components/pages/ResultPage.tsx
@@ -2,37 +2,19 @@ import { useRecoilValue } from "recoil";
 import { Descriptions } from "antd";
 import { styled } from "styled-components";
 
-import { wrongAnswerQuestionsCountState } from "../../recoil";
-import { useUserIdRedirect } from "../../hooks/useUserIdRedirect";
+import { resultTableItemsState } from "../../recoil";
 
 const ResultPage = () => {
-  const wrongAnswerQuestionsCount = useRecoilValue(
-    wrongAnswerQuestionsCountState
-  );
-
-  const { userId, navigate } = useUserIdRedirect();
-
-  const wrongCount = wrongAnswerQuestionsCount;
-
-  const correctCount = 10 - wrongAnswerQuestionsCount;
-
-  // const descriptionsItems = [];
+  const resultTableItems = useRecoilValue(resultTableItemsState);
 
   return (
     <Box>
       <Descriptions column={6} title="퀴즈 결과표" bordered>
-        <Descriptions.Item span={6} label="아이디">
-          {userId}
-        </Descriptions.Item>
-        <Descriptions.Item span={3} label="정답 수">
-          {correctCount}개
-        </Descriptions.Item>
-        <Descriptions.Item span={3} label="오답 수">
-          {wrongCount}개
-        </Descriptions.Item>
-        <Descriptions.Item span={6} label="소요 시간">
-          8분 31초
-        </Descriptions.Item>
+        {resultTableItems.map(({ span, label, content }) => (
+          <Descriptions.Item span={span} label={label}>
+            {content}
+          </Descriptions.Item>
+        ))}
       </Descriptions>
     </Box>
   );
