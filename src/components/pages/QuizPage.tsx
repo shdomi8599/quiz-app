85fe39254a681b1507606855976cd295cbc35c26
shdomi8599 <shdomi@naver.com>
2023-06-14 20:18:33 +0900

    feat : 타이머, progress, ResultTableItems 데이터 관리

--- src/components/pages/QuizPage.tsx
+++ src/components/pages/QuizPage.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useRecoilState } from "recoil";
-import { styled } from "styled-components";
+import { css, keyframes, styled } from "styled-components";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { decode } from "he";
 import {
   Button,
@@ -13,7 +13,13 @@ import {
   Progress,
 } from "antd";
 
-import { quizDatasState, wrongAnswerQuestionsState } from "../../recoil";
+import {
+  elapsedTimeState,
+  quizDatasState,
+  resultTableItemsState,
+  wrongAnswerQuestionsCountState,
+  wrongAnswerQuestionsState,
+} from "../../recoil";
 import { getDbDataByDocName, setDbData } from "../../util/firebase";
 import { shuffleDatas } from "../../util/random";
 import { UserData } from "../../types";
@@ -28,15 +34,42 @@ const QuizPage = () => {
 
   const { id } = useParams();
 
+  const secLimit = 30;
+
+  const [sec, setSec] = useState(secLimit);
+
   const [isViewAnswer, setIsViewAnswer] = useState(false);
 
   const [selectedAnswer, setSelectedAnswer] = useState("");
+
+  const [, setResultTableItems] = useRecoilState(resultTableItemsState);
+
+  const wrongAnswerQuestionsCount = useRecoilValue(
+    wrongAnswerQuestionsCountState
+  );
+
+  const [elapsedTime, setElapsedTime] = useRecoilState(elapsedTimeState);
 
   const [wrongAnswerQuestions, setWrongAnswerQuestions] = useRecoilState(
     wrongAnswerQuestionsState
   );
 
-  const [quizDatas] = useRecoilState(quizDatasState);
+  const quizDatas = useRecoilValue(quizDatasState);
+
+  const progressBarPercent = (sec * 10) / 3;
+
+  const progressBarColor = () => {
+    if (isViewAnswer) {
+      return "gray";
+    }
+    if (sec <= 10) {
+      return "red";
+    }
+    if (sec <= 20) {
+      return "#f3f039";
+    }
+    return;
+  };
 
   const quizId = Number(id);
 
@@ -81,6 +114,18 @@ const QuizPage = () => {
   const navigateToResultPage = async () => {
     handleCommonLoading();
 
+    const wrongCount = wrongAnswerQuestionsCount;
+
+    const correctCount = 10 - wrongAnswerQuestionsCount;
+
+    const newResultTableItems = [
+      { label: "아이디", span: 6, content: userId },
+      { label: "정답 수", span: 3, content: `${correctCount}개` },
+      { label: "오답 수", span: 3, content: `${wrongCount}개` },
+      { label: "소요 시간", span: 6, content: `${elapsedTime}초 / 600초` },
+    ];
+    setResultTableItems(newResultTableItems);
+
     await getDbDataByDocName<UserData>("users", userId)
       .then((res) => {
         const resultsData = res.results;
@@ -96,7 +141,7 @@ const QuizPage = () => {
 
         setDbData("users", userId, newData);
       })
-      .catch(() => errorAlert("잠시 후에 다시 시도해주세요.", "결과"));
+      .catch(() => errorAlert("잠시 후에 다시 시도해주세요.", "결과 조회"));
 
     handleCommonLoading();
 
@@ -104,17 +149,53 @@ const QuizPage = () => {
   };
 
   useEffect(() => {
+    if (isViewAnswer) {
+      const newElapsedTime = secLimit - sec;
+      setElapsedTime((prev) => prev + newElapsedTime);
+    }
+
     const isWrongAnswer =
       selectedAnswer !== "" && selectedAnswer !== correctAnswer;
+
     if (isWrongAnswer && isViewAnswer) {
       setWrongAnswerQuestions((prev) => [...prev, quizData]);
     }
   }, [isViewAnswer]);
 
+  useEffect(() => {
+    setSec(secLimit);
+  }, [quizId]);
+
+  useEffect(() => {
+    if (isViewAnswer) {
+      return;
+    }
+
+    if (sec === 0) {
+      return setIsViewAnswer(true);
+    }
+
+    const interval = setInterval(() => {
+      setSec((prev) => prev - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [sec, isViewAnswer]);
+
   return (
     <Box $isViewAnswer={isViewAnswer}>
       <Progress type="circle" percent={circleProgressPercent} />
-      <Progress className="progress" percent={3} status="active" />
+      <ProgressBarBox $sec={sec}>
+        <Progress
+          className="progress-bar"
+          percent={progressBarPercent}
+          status="active"
+          strokeColor={progressBarColor()}
+        />
+        <span className="custom-sec">{sec}초</span>
+      </ProgressBarBox>
       <Card className="card" title={<div>{quizTitle}</div>} bordered={false}>
         <Radio.Group onChange={handleSelectedAnswer} value={selectedAnswer}>
           <Space direction="vertical">
@@ -162,11 +243,6 @@ const Box = styled.main<BoxProps>`
   flex-direction: column;
   gap: 40px;
 
-  .progress {
-    width: 70%;
-    margin: 0;
-  }
-
   .card {
     width: 70%;
     height: auto;
@@ -184,5 +260,49 @@ const Box = styled.main<BoxProps>`
 
   .radio-box {
     margin: 20px 40px;
+  }
+`;
+
+const vibrationAnimation = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-1.2px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
+type ProgressBarBoxProps = {
+  $sec: number;
+};
+
+const ProgressBarBox = styled.div<ProgressBarBoxProps>`
+  width: 70%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  .progress-bar {
+    width: 100%;
+    margin: 0;
+    animation: ${({ $sec }) =>
+      $sec <= 10 && $sec > 0
+        ? css`
+            ${vibrationAnimation} 0.2s ease-in-out infinite
+          `
+        : "none"};
+
+    > span {
+      display: none;
+    }
+  }
+
+  .custom-sec {
+    position: absolute;
+    right: 0px;
   }
 `;
