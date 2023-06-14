0c5352e07f45e0bf8d520cd93fb09366c35027a4
shdomi8599 <shdomi@naver.com>
2023-06-14 22:39:47 +0900

    feat : CopyBtn 활용, confetti 애니메이션 활용

--- src/components/pages/ResultPage.tsx
+++ src/components/pages/ResultPage.tsx
@@ -1,24 +1,55 @@
+import { useEffect } from "react";
 import { useRecoilValue } from "recoil";
-import { Descriptions } from "antd";
+import { Descriptions, Result } from "antd";
+import { SmileOutlined } from "@ant-design/icons";
 import { styled } from "styled-components";
 
-import { resultTableItemsState } from "../../recoil";
+import {
+  resultTableItemsState,
+  wrongAnswerQuestionsCountState,
+} from "../../recoil";
+import { confettiRealisticLook, confettiStar } from "../../util/confetti";
 import { useUserIdRedirect } from "../../hooks/useUserIdRedirect";
+
+import CopyBtn from "../common/CopyBtn";
 
 const ResultPage = () => {
   useUserIdRedirect();
 
+  const wrongCount = useRecoilValue(wrongAnswerQuestionsCountState);
+
   const resultTableItems = useRecoilValue(resultTableItemsState);
+
+  useEffect(() => {
+    if (wrongCount === 0) {
+      return confettiStar();
+    }
+    confettiRealisticLook();
+  }, []);
 
   return (
     <Box>
-      <Descriptions column={6} title="퀴즈 결과표" bordered>
-        {resultTableItems.map(({ span, label, content }) => (
-          <Descriptions.Item key={label} span={span} label={label}>
-            {content}
-          </Descriptions.Item>
-        ))}
-      </Descriptions>
+      <Result
+        icon={<SmileOutlined />}
+        title={
+          <>
+            <div>축하합니다! 모든 퀴즈를 완료하셨습니다.</div>
+            <div className="sub-title">
+              아이디를 저장해두시면 다양한 컨텐츠에 활용하실 수 있습니다.
+            </div>
+          </>
+        }
+        extra={
+          <Descriptions column={6} title="결과표" bordered>
+            {resultTableItems.map(({ span, label, content }) => (
+              <Descriptions.Item key={label} span={span} label={label}>
+                {content}
+                {label === "아이디" && <CopyBtn content={content} />}
+              </Descriptions.Item>
+            ))}
+          </Descriptions>
+        }
+      />
     </Box>
   );
 };
@@ -28,4 +59,14 @@ export default ResultPage;
 const Box = styled.main`
   display: flex;
   flex-direction: column;
+
+  .sub-title {
+    margin-top: 20px;
+    font-size: 1.4rem;
+    font-weight: 800;
+  }
+
+  .copy-btn {
+    margin-left: 8px;
+  }
 `;
