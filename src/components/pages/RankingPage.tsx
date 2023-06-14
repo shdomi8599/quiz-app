308a215e7bd76e24f8d6f20cb116877baaa3ed16
shdomi8599 <shdomi@naver.com>
2023-06-15 03:50:37 +0900

    feat : 랭킹 페이지 기본 구성

--- src/components/pages/RankingPage.tsx
+++ src/components/pages/RankingPage.tsx
@@ -1,18 +1,57 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { styled } from "styled-components";
 import { UserData } from "../../types";
 import { getDbAllData } from "../../util/firebase";
+import { useCommonLoading } from "../../hooks/useCommonLoading";
+import TableSkeleton from "../skeleton/TableSkeleton";
+import TableContent from "../table/TableContent";
+import { RANKING_COLUMNS } from "../../constants";
 
 const RankingPage = () => {
+  const { commonLoading, handleCommonLoading } = useCommonLoading();
+
+  const [page, setPage] = useState(1);
+
+  const [size, setSize] = useState(10);
+
   const [userDatas, setUserDatas] = useState<UserData[]>([]);
 
+  const startIndex = (page - 1) * size;
+  const endIndex = startIndex + size;
+
+  const data = userDatas.slice(startIndex, endIndex);
+
+  const totalElements = userDatas.length;
+
+  const handlePageChange = useCallback((newPage: number, newSize: number) => {
+    setPage(newPage);
+    setSize(newSize);
+  }, []);
+
   useEffect(() => {
+    handleCommonLoading();
     getDbAllData<UserData>("users").then((res) => {
       setUserDatas(res);
+      handleCommonLoading();
     });
   }, []);
 
-  return <Box>랭킹페이지입니다.</Box>;
+  return (
+    <Box>
+      {commonLoading ? (
+        <TableSkeleton size={size} />
+      ) : (
+        <TableContent<UserData>
+          dataSource={data}
+          columns={RANKING_COLUMNS}
+          page={page}
+          size={size}
+          handlePageChange={handlePageChange}
+          totalElements={totalElements}
+        />
+      )}
+    </Box>
+  );
 };
 
 export default RankingPage;
