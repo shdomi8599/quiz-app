33ae69a34e37e81f72389e3e6c1e7c9e835e3ed3
shdomi8599 <shdomi@naver.com>
2023-06-14 12:08:21 +0900

    feat : shuffleDatas 함수

--- src/util/random/index.ts
+++ src/util/random/index.ts
@@ -6,3 +6,11 @@ export const generateRandomCode = (count: number) => {
   }
   return `#${randomCode}`;
 };
+
+export const shuffleDatas = <T>(array: T[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
