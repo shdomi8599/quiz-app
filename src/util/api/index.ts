eb070ffd8b81cd4cfe9e5ae0fc5d34d7538dd879
shdomi8599 <shdomi@naver.com>
2023-06-15 01:37:52 +0900

    refactor : getQuizDatas catch 함수 삭제

--- src/util/api/index.ts
+++ src/util/api/index.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 import { QuizData } from "../../types";
-import { errorAlert } from "../../components/common/Alert";
 
 const baseURL = "https://opentdb.com";
 
@@ -14,7 +13,7 @@ export const quizApi = axios.create({
 });
 
 export const getQuizDatas = async (size: number): Promise<QuizData[]> => {
-  return await quizApi(`api.php?amount=${size}`)
-    .then((res) => res.data.results)
-    .catch(() => errorAlert("잠시 후에 다시 시도해주세요.", "퀴즈"));
+  return await quizApi(`api.php?amount=${size}`).then(
+    (res) => res.data.results
+  );
 };
