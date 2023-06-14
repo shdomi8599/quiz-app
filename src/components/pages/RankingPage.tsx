35e62a8a929e5e8329905abec7ec1ab0f56f38e0
shdomi8599 <shdomi@naver.com>
2023-06-15 03:31:52 +0900

    feat : state setUserDatas

--- src/components/pages/RankingPage.tsx
+++ src/components/pages/RankingPage.tsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
 import { styled } from "styled-components";
+import { UserData } from "../../types";
+import { getDbAllData } from "../../util/firebase";
 
 const RankingPage = () => {
+  const [userDatas, setUserDatas] = useState<UserData[]>([]);
+
+  useEffect(() => {
+    getDbAllData<UserData>("users").then((res) => {
+      setUserDatas(res);
+    });
+  }, []);
+
   return <Box>랭킹페이지입니다.</Box>;
 };
 
