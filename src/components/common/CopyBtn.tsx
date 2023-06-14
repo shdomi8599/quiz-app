0839c2d1dc0438e1113ab0f115f1102de3307144
shdomi8599 <shdomi@naver.com>
2023-06-14 22:38:58 +0900

    feat : CopyBtn 컴포넌트

---
+++ src/components/common/CopyBtn.tsx
@@ -0,0 +1,25 @@
+import { Button } from "antd";
+import { errorAlert, successAlert } from "./Alert";
+
+type Props = {
+  content: string;
+};
+
+const CopyBtn = ({ content }: Props) => {
+  const doCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(content);
+      successAlert("클립보드에 복사되었습니다.", "아이디 복사");
+    } catch (error) {
+      errorAlert("다시 시도해주세요.", "아이디 복사");
+    }
+  };
+
+  return (
+    <Button className="copy-btn" onClick={doCopy}>
+      복사
+    </Button>
+  );
+};
+
+export default CopyBtn;
