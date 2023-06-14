c1e5139407e0ab659cd340f219af08bd64214e9c
shdomi8599 <shdomi@naver.com>
2023-06-14 12:09:17 +0900

    feat : useUserIdRedirect hook

---
+++ src/hooks/useUserIdRedirect.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import { useNavigate } from "react-router-dom";
+
+import { userIdState } from "../recoil";
+
+export const useUserIdRedirect = () => {
+  const userId = useRecoilValue(userIdState);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userId) {
+      navigate("/");
+    }
+  }, [userId]);
+
+  return {
+    userId,
+    navigate,
+  };
+};
