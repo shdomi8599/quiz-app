8486253abb26e47a60c612ba33770d60708ff3d9
shdomi8599 <shdomi@naver.com>
2023-06-16 05:41:06 +0900

    feat : 재도전을 하기위한 데이터 세팅

--- src/components/pages/RetryPage.tsx
+++ src/components/pages/RetryPage.tsx
@@ -1,44 +1,46 @@
 import { useEffect } from "react";
-import { styled } from "styled-components";
 
 import { useGetUserData } from "../../hooks/user/useGetUserData";
 import { useSetQuizAppState } from "../../hooks/useSetQuizAppState";
 
 import RecordsSearchForm from "../form/RecordsSearchForm";
+import { useNavigate } from "react-router-dom";
 
 const RetryPage = () => {
-  const {
-    userId,
-    resultsData,
-    selectedResult,
-    onFinish,
-    handleLoading,
-    handleResultChange,
-  } = useGetUserData();
+  const navigate = useNavigate();
+
+  const { userId, resultsData, selectedResult, onFinish, handleResultChange } =
+    useGetUserData();
 
   const { setCurrentNavItem, setUserId, setQuizDatas, setQuizLevel } =
     useSetQuizAppState();
 
   useEffect(() => {
-    if (resultsData && selectedResult) {
-      const targetResultData = resultsData[selectedResult];
+    if (resultsData) {
+      const targetResultData = resultsData[selectedResult as number];
+
       const { wrongAnswerQuestions } = targetResultData;
+
+      setUserId(userId as string);
+
+      setQuizDatas(wrongAnswerQuestions);
+
+      setQuizLevel(wrongAnswerQuestions.length);
+
       setCurrentNavItem("재도전");
+
+      navigate("/quiz/1");
     }
   }, [selectedResult]);
 
   return (
-    <>
-      <RecordsSearchForm
-        resultsData={resultsData}
-        handleResultChange={handleResultChange}
-        btnName="목록 조회"
-        onFinish={onFinish}
-      />
-    </>
+    <RecordsSearchForm
+      resultsData={resultsData}
+      handleResultChange={handleResultChange}
+      btnName="목록 조회"
+      onFinish={onFinish}
+    />
   );
 };
 
 export default RetryPage;
-
-const Box = styled.main``;
