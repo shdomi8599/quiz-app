bead32c22e636a55b21c9919dc936518c2900e34
shdomi8599 <shdomi@naver.com>
2023-06-16 18:04:43 +0900

    feat : RecordsSearchForm에 메인 폼 추가

--- src/components/form/RecordsSearchForm.tsx
+++ src/components/form/RecordsSearchForm.tsx
@@ -26,6 +26,14 @@ const RecordsSearchForm = ({
 
   const isRetryPage = pathname.includes("retry");
 
+  const mainResultTitle = isRetryPage
+    ? "재도전을 시도해보세요!"
+    : "오답노트를 작성해주세요!";
+
+  const selectResultTitle = isRetryPage
+    ? "날짜를 선택하면 바로 재도전이 시작됩니다."
+    : "날짜를 선택하면 오답노트를 작성할 수 있습니다.";
+
   const selectWord = isRetryPage ? "도전" : "조회";
 
   const selectItemsOptions = useMemo(
@@ -48,40 +56,35 @@ const RecordsSearchForm = ({
   return (
     <Box>
       {selectItemsOptions ? (
-        <>
-          {isRetryPage ? (
-            <Result
-              status="warning"
-              title={"날짜를 선택하면 바로 재도전이 시작됩니다."}
-              extra={SelectBox}
-            />
-          ) : (
-            SelectBox
-          )}
-        </>
+        <Result status="warning" title={selectResultTitle} extra={SelectBox} />
       ) : (
-        <Form autoComplete="off" onFinish={onFinish}>
-          <Form.Item name="nickname" rules={FORM_NICKNAME_RULES}>
-            <Input maxLength={10} placeholder="닉네임을 입력해주세요." />
-          </Form.Item>
-          <Form.Item
-            name="code"
-            rules={[
-              {
-                required: true,
-                message: "#을 포함한 숫자를 입력해야 조회할 수 있습니다.",
-              },
-            ]}
-          >
-            <Input
-              maxLength={5}
-              placeholder="#을 포함한 숫자 4자리를 입력해주세요."
-            />
-          </Form.Item>
-          <Button type="primary" htmlType="submit">
-            {btnName}
-          </Button>
-        </Form>
+        <Result
+          title={mainResultTitle}
+          extra={
+            <Form autoComplete="off" onFinish={onFinish}>
+              <Form.Item name="nickname" rules={FORM_NICKNAME_RULES}>
+                <Input maxLength={10} placeholder="닉네임을 입력해주세요." />
+              </Form.Item>
+              <Form.Item
+                name="code"
+                rules={[
+                  {
+                    required: true,
+                    message: "#을 포함한 숫자를 입력해야 조회할 수 있습니다.",
+                  },
+                ]}
+              >
+                <Input
+                  maxLength={5}
+                  placeholder="#을 포함한 숫자 4자리를 입력해주세요."
+                />
+              </Form.Item>
+              <Button type="primary" htmlType="submit">
+                {btnName}
+              </Button>
+            </Form>
+          }
+        />
       )}
     </Box>
   );
@@ -89,19 +92,7 @@ const RecordsSearchForm = ({
 export default RecordsSearchForm;
 
 const Box = styled.main`
-  display: flex;
-  flex-direction: column;
-  gap: 40px;
-  margin: 20px 0px;
-
-  .select {
-    width: 90%;
-    max-width: 500px;
-  }
-
   form {
-    max-width: 350px;
-    width: 90%;
     display: flex;
     flex-direction: column;
     gap: 10px;
